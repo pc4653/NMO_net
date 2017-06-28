23edfc05344014019292b24984cdf9a485a12183
Cheng Peng <chengp4653@gmail.com>
2017-06-27 22:29:12 -0400

    updated

--- export_procedure_v3.py
+++ export_procedure_v3.py
@@ -12,14 +12,6 @@ output_path = '/home/chengp/Pictures/NMO_by_Patient/'
 
 input_dir = '/home/chengp/Pictures/NMO_Patient/'
 
-with tf.gfile.FastGFile("/home/chengp/Pictures/scripts/Multi-label-Inception-net/retrained_graph.pb", 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
-
-sess = tf.Session()
-softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-
 os.chdir(input_dir)
 path = os.getcwd()
 filenames = os.listdir(path)
@@ -31,30 +23,7 @@ for filename in filenames:
    filenames1 = os.listdir(path1)
    for filename1 in filenames1:
      input_path = input_data + filename1
-     [desc, subpath] = read_export(input_path, output_path)
-     position_lock = 1
-     orientation_lock = 1
-     mode_lock = 1
-     image_data = tf.gfile.FastGFile('temp.jpg', 'rb').read()
-
-     predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
-    
-      # Sort to show labels of first prediction in order of confidence
-     top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-     for node_id in top_k:
-      if (node_id in (0, 1)) and position_lock:
-          position = label_lines[node_id]
-          position_lock = 0
-      elif node_id in (2, 3, 4) and orientation_lock:
-          orientation = label_lines[node_id]
-          orientation_lock = 0
-      elif node_id in (5, 6, 7, 8, 9, 10, 11, 12, 13, 14) and mode_lock:
-          mode = label_lines[node_id]
-          mode_lock = 0
-     net_result = position+'#'+orientation+'#'+mode 
-     name = net_result + '%' + desc + '.jpg'
-     output_string = os.path.join(subpath,name)
-     os.system('mv temp.jpg ' + output_string)
+     read_export(input_path, output_path)
 
 
 
--- label_image.py
+++ label_image.py
@@ -1,44 +1,52 @@
 import tensorflow as tf
 import sys
+import os
 
-# change this as you see fit
-image_path = sys.argv[1]
-
-# Read in the image_data
-image_data = tf.gfile.FastGFile(image_path, 'rb').read()
-
-# Loads label file, strips off carriage return
-label_lines = [line.rstrip() for line 
-                   in tf.gfile.GFile("labels.txt")]
+input_dir = '/home/chengp/Pictures/NMO_by_Patient/'
+tags = open('/home/chengp/Pictures/labels.txt').readlines()
+label_lines = map(str.strip, tags)
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+with tf.gfile.FastGFile("/home/chengp/Pictures/scripts/Multi-label-Inception-net/retrained_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
 with tf.Session() as sess:
     # Feed the image_data as input to the graph and get first prediction
-    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    
-    predictions = sess.run(softmax_tensor, \
-             {'DecodeJpeg/contents:0': image_data})
-    
-    # Sort to show labels of first prediction in order of confidence
-    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-    
-    for node_id in top_k:
-        human_string = label_lines[node_id]
-        score = predictions[0][node_id]
-        print('%s (score = %.5f)' % (human_string, score))
-    
-
-    filename = "results.txt"    
-    with open(filename, 'a+') as f:
-        f.write('\n**%s**\n' % (image_path))
-        for node_id in top_k:
-            human_string = label_lines[node_id]
-            score = predictions[0][node_id]
-            f.write('%s (score = %.5f)\n' % (human_string, score))
-    
-    
+	softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+	IDs = os.listdir(input_dir)
+	for ID in IDs:
+		path = os.path.join(input_dir, ID)
+		dates = os.listdir(path)
+		for date in dates:
+			path1 = os.path.join(path, date)
+			series_numbers = os.listdir(path1)
+			for series_number in series_numbers:
+					path2 = os.path.join(path1, series_number)
+					filenames = os.listdir(path2)
+					for filename in filenames:
+						absolute_path = os.path.join(path2, filename)
+              			# Read in the image_data
+						image_data = tf.gfile.FastGFile(absolute_path, 'rb').read()
+						predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+						# Sort to show labels of first prediction in order of confidence
+						top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+						
+						position_lock = 1
+						orientation_lock = 1
+						mode_lock = 1
+						for node_id in top_k:
+							if (node_id in (0, 1)) and position_lock:
+								position = label_lines[node_id]
+								position_lock = 0 
+							elif node_id in (2, 3, 4) and orientation_lock:
+								orientation = label_lines[node_id]
+								orientation_lock = 0
+							elif node_id in (5, 6, 7, 8, 9, 10, 11, 12, 13, 14) and mode_lock:
+								mode = label_lines[node_id]
+								mode_lock = 0
+						net_result = position+'#'+orientation+'#'+mode + '#'
+						new_name = net_result + filename
+						new_path = os.path.join(path2, new_name)
+						os.rename(absolute_path, new_path)
