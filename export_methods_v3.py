9c636ca7b8e8c254698e94799c9088c280bb40d5
Cheng Peng <chengp4653@gmail.com>
2017-06-29 10:37:05 -0400

    change naming scheme for folders

--- export_methods_v3.py
+++ export_methods_v3.py
@@ -4,14 +4,14 @@ import dicom
 group = 'NMO'
 
 def category( desc ):
-   if 'head'.lower() in desc.lower():
-     result = 1;
-   elif 'spine' in desc.lower():
-     result = 1;
-   else:
-     result = 0;
+	if 'head'.lower() in desc.lower():
+		result = 1;
+	elif 'spine' in desc.lower():
+		result = 1;
+	else:
+		result = 0;
 
-   return result;
+	return result;
 
 
 #def tagfile( name, tags ):
@@ -22,71 +22,81 @@ def category( desc ):
 #   f.close()
 
 def read_export(input_str, output_path):
-   try: 
-     data = dicom.read_file(input_str)
-   except dicom.errors.InvalidDicomError:
-     print 'InvalidDicomError'
-     print input_str
-     return;
-   ID = str(data.PatientID)
-   try:
-	   desc = data.StudyDescription + '#' + data.SeriesDescription
-	   if ' ' in desc:
-	     desc = desc.replace(' ', '_')
-	   if '&' in desc:
-	     desc = desc.replace('&','and')
-	   if '/' in desc:
-	     desc = desc.replace('/','or')
-   except AttributeError:
-           print 'No Description'
-	   print input_str
-	   desc = 'missing'
-
-#   number = str(data.SeriesNumber) + '#' + str(data.InstanceNumber)
+	try: 
+		data = dicom.read_file(input_str)
+	except dicom.errors.InvalidDicomError:
+		print 'InvalidDicomError'
+		print input_str
+		return;
+	ID = str(data.PatientID)
+	try:
+		study_desc = data.StudyDescription
+		if ' ' in study_desc:
+			study_desc = study_desc.replace(' ', '_')
+		if '&' in study_desc:
+			study_desc = study_desc.replace('&','and')
+		if '/' in study_desc:
+			study_desc = study_desc.replace('/','or')
+	except AttributeError:
+		print 'No Study Description'
+		print input_str
+		study_desc = 'missing'
+		
+		
+	try:
+		series_desc = data.SeriesDescription
+		if ' ' in series_desc:
+			series_desc = series_desc.replace(' ', '_')
+		if '&' in series_desc:
+			series_desc = series_desc.replace('&','and')
+		if '/' in series_desc:
+			series_desc = series_desc.replace('/','or')
+	except AttributeError:
+		print 'No Series Description'
+		print input_str
+		series_desc = 'missing'
 	   
 
-   try:
-     date = str(data.AcquisitionDate)
-   except AttributeError:
-     print 'NO AcquisitionDate'
-     print input_str
-     try: 
-       date = str(data.StudyDate)
-     except AttributeError:
-       print 'NO StudyDate'
-       date = 'missing'
+	try:
+		date = str(data.AcquisitionDate)
+	except AttributeError:
+		print 'NO AcquisitionDate'
+		print input_str
+		try: 
+			date = str(data.StudyDate)
+		except AttributeError:
+			print 'NO StudyDate'
+			date = 'missing'
 
+	study_date = study_desc + '[' + date + ']'
+	name = '%' + str(data.InstanceNumber) + '.jpg'
+	#result = category(desc)
 
-   name = desc + '%%%' + str(data.InstanceNumber) + '.jpg'
-   result = category(desc)
+	subpath = os.path.join(output_path, ID)
+	if not os.path.isdir(subpath):
+		os.mkdir(subpath)
 
-   subpath = os.path.join(output_path, ID)
-   if not os.path.isdir(subpath):
-     os.mkdir(subpath)
+	subpath = os.path.join(subpath, study_date)
+	if not os.path.isdir(subpath):
+		os.mkdir(subpath)
 
-   subpath = os.path.join(subpath, date)
-   if not os.path.isdir(subpath):
-     os.mkdir(subpath)
+	subpath = os.path.join(subpath,str(data.SeriesNumber) + '%%' + series_desc)
+	if not os.path.isdir(subpath):
+		os.mkdir(subpath)
 
-   subpath = os.path.join(subpath,str(data.SeriesNumber))
-   if not os.path.isdir(subpath):
-     os.mkdir(subpath)
-
-   output_string = os.path.join(subpath,name)
-   if result == 1:
-     #tagfile(name, tags)
-    if not os.path.isfile(output_string):
-       try:
-         scipy.misc.toimage(data.pixel_array, cmin=0, cmax=255).save(output_string)
-       except ValueError:
-         print 'ValueError'
-         print input_str
-       except TypeError:
-         print 'TypeError'
-         print input_str 
+	output_string = os.path.join(subpath,name)
+	if not os.path.isfile(output_string):
+		try:
+			scipy.misc.toimage(data.pixel_array, cmin=0, cmax=255).save(output_string)
+		except ValueError:
+			print 'ValueError'
+			print input_str
+		except TypeError:
+			print 'TypeError'
+			print input_str 
    
 
 
 
 
-   return;
+	return;
